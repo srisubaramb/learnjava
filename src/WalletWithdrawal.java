b72d6851973141d6b6c4fb2675691a30863495f7
srisubaramb <srisubaramb@gmail.com>
2026-08-27 10:03:48 +0530

    variables, data types, class, main , decision, formats printing

---
+++ src/WalletWithdrawal.java
@@ -0,0 +1,17 @@
+public class WalletWithdrawal {
+    public static void main(String[] agrs){
+        String userName = "Haauen";
+        float balance = 100.0f;
+        float withdraw = 101.0f;
+        System.out.printf("____WELCOME %s____" , userName);
+        if (withdraw <= balance) {
+            balance -= withdraw;
+            System.out.printf("\nWithdrawal successful : %.2f\nAvailable balance : %.2f" ,
+                    withdraw, balance);
+        } else {
+            System.out.printf("\nInsufficient balance\nAvailable balance %.2f" , balance);
+        }
+    }
+}
+//this program cover variable , data type , class , main method signature,
+// decision / condition logic (if , else if , else) , printing statements , formats in printing
